7ccfca70fcb887155749a52af0f35604bc81f151
Tungtungsahur28 <vinhhuuke190583@gmail.com>
2025-08-30 16:00:06 +0700

    Update Scriptdemo.lua

--- Scriptdemo.lua
+++ Scriptdemo.lua
@@ -22,7 +22,7 @@ loadstring(game:HttpGet(("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V
 
        MinimizeButton({
        Image = "http://www.roblox.com/asset/?id=127080972250886",
-       Size = {120, 60},
+       Size = {60, 60},
        Color = Color3.fromRGB(10, 10, 10),
        Corner = true,
        Stroke = false,
